0c96d7b3db9c5d5e15e0ed8d63e66de10332883f
Green Lightning <GreenLightning.git@googlemail.com>
2020-12-13 13:08:49 +0100

    Add day 13

---
+++ day13/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type bus struct{ index, id int }
+
+func main() {
+	lines := readLines("input.txt")
+	ready := toInt(lines[0])
+
+	var list []bus
+	for i, entry := range strings.Split(lines[1], ",") {
+		if entry != "x" {
+			id := toInt(entry)
+			list = append(list, bus{i % id, id})
+		}
+	}
+
+	{
+		fmt.Println("--- Part One ---")
+		earliestDelay, earliestID := math.MaxInt32, -1
+		for _, bus := range list {
+			delay := (bus.id - ready%bus.id) % bus.id
+			if delay < earliestDelay {
+				earliestDelay, earliestID = delay, bus.id
+			}
+		}
+		fmt.Println(earliestDelay * earliestID)
+	}
+
+	{
+		fmt.Println("--- Part Two ---")
+		multiplier, offset := 1, 1
+		for _, bus := range list {
+			for k := 0; ; k++ {
+				t := k*multiplier + offset
+				delay := (bus.id - t%bus.id) % bus.id
+				if delay == bus.index {
+					multiplier *= bus.id
+					offset = t
+					break
+				}
+			}
+		}
+		fmt.Println(offset)
+	}
+}
+
+func readLines(filename string) []string {
+	file, err := os.Open(filename)
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
+
+func toInt(s string) int {
+	result, err := strconv.Atoi(s)
+	check(err)
+	return result
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
